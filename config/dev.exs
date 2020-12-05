c4ea922d7b2e8f3d1aa81d60dc1702bb8028b8dd
Chris Howard <chrishoward91@gmail.com>
2020-12-05 14:01:17 +0000

    chore: Make app runnable locally

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :weight_track, WeightTrack.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "password",
   database: "weight_track_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
